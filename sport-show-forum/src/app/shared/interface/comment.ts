17a00d71f1b1802b90cbb039dcb9ab957ad09c18
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-05 23:11:16 +0300

    build

--- sport-show-forum/src/app/shared/interface/comment.ts
+++ sport-show-forum/src/app/shared/interface/comment.ts
@@ -1,2 +1,10 @@
+import { Post } from "./post";
+
 export interface Comment {
+    _id: string;
+    username: string;
+    content: string;
+    likes: string[];
+    postId: Post;
+    created_at: string;
 }
--- sport-show-forum/src/app/shared/interface/post.ts
+++ sport-show-forum/src/app/shared/interface/post.ts
@@ -1,2 +1,15 @@
+import { User } from "./user";
+import { Comment } from "./comment";
+
 export interface Post {
+    keyword: string;
+    title: string;
+    content: string;
+    _id: string;
+    user: User;
+    img?: string;
+    likes: string[];
+    comments: Comment[];
+    created_at: string;
+    updatedAt: string;
 }
