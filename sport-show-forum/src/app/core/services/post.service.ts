a1466de70e908ca8a2aaa9db7f55a6ce44155d51
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-05 23:05:10 +0300

    build

--- sport-show-forum/src/app/core/services/post.service.ts
+++ sport-show-forum/src/app/core/services/post.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(public firestore: Firestore, public router: Router, public route: ActivatedRoute) { }
+
 }
