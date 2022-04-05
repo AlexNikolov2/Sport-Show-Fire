20782bf5e03b99a3ac7f5c4c9c5cf577089b1ade
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-05 21:45:50 +0300

    build

--- sport-show-forum/src/environments/environment.prod.ts
+++ sport-show-forum/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'sport-show-forum',
+    appId: '1:701281477970:web:5698956cf716dca70dc68b',
+    databaseURL: 'https://sport-show-forum-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'sport-show-forum.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyD4Lz3F7WNiHYvx7w_r1VjAZhbZZhXkum4',
+    authDomain: 'sport-show-forum.firebaseapp.com',
+    messagingSenderId: '701281477970',
+    measurementId: 'G-7JWST5MQQY',
+  },
   production: true
 };
--- sport-show-forum/src/environments/environment.ts
+++ sport-show-forum/src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'sport-show-forum',
+    appId: '1:701281477970:web:5698956cf716dca70dc68b',
+    databaseURL: 'https://sport-show-forum-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'sport-show-forum.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyD4Lz3F7WNiHYvx7w_r1VjAZhbZZhXkum4',
+    authDomain: 'sport-show-forum.firebaseapp.com',
+    messagingSenderId: '701281477970',
+    measurementId: 'G-7JWST5MQQY',
+  },
   production: false
 };
 
