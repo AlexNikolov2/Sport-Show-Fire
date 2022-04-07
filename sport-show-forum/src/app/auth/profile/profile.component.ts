48d04b30eb1d550f95d06b0cd3b10dac19ccbd54
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-07 16:11:55 +0300

    build

--- sport-show-forum/src/app/auth/profile/profile.component.ts
+++ sport-show-forum/src/app/auth/profile/profile.component.ts
@@ -21,9 +21,6 @@ export class ProfileComponent implements OnInit {
     this.auth.currentUser.then(user => {
       this.email = user?.email;
     });
-    this.postService.getPosts()!.subscribe(posts => {
-      this.posts = posts;
-    });
   }
 
 }
