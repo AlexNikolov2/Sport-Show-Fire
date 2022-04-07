7308f21e22ae63a84777d6ebe0162ae378440208
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-07 17:34:06 +0300

    build

--- sport-show-forum/src/app/items/items-routing.module.ts
+++ sport-show-forum/src/app/items/items-routing.module.ts
@@ -5,12 +5,13 @@ import { AllPostsComponent } from './all-posts/all-posts.component';
 import { EditPostComponent } from './edit-post/edit-post.component'
 import { PostDetailsComponent } from './post-details/post-details.component';
 import { AuthGuard } from '../core/guards/auth.guard';
+import { OwnerGuard } from '../core/guards/owner.guard';
 
 const routes: Routes = [
   { path: 'all-posts', component: AllPostsComponent, canActivate: [AuthGuard], data: { authRequired: false } },
   { path: 'create', component: CreatePostComponent, canActivate: [AuthGuard], data: { authRequired: true } },
-  { path: 'edit/:id', component: EditPostComponent },
-  { path: 'details/:id', component: PostDetailsComponent }
+  { path: 'edit/:id', component: EditPostComponent, canActivate: [OwnerGuard], data: { authRequired: true } },
+  { path: 'details/:id', component: PostDetailsComponent, canActivate: [AuthGuard], data: { authRequired: false } },
 ];
 @NgModule({
   imports: [
