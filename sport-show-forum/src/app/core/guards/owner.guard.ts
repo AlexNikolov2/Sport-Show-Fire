708881f16109e0c754f41bcb74e135daaa7da10e
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-11 20:49:18 +0300

    build

--- sport-show-forum/src/app/core/guards/owner.guard.ts
+++ sport-show-forum/src/app/core/guards/owner.guard.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { PostService } from '../services/post.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OwnerGuard implements CanActivate {
+
+  constructor(
+    public auth: AngularFireAuth,
+    public postService: PostService,
+    public router: Router
+  ) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
+    const id = route.params['id'];
+      return this.postService.getPostById(id).toPromise().then((res: any) => {
+        const post: any = res.data();
+        return this.auth.currentUser.then(res => {
+          if (post.user == res?.email) {
+            return true;
+          } else {
+            this.router.navigate([`/details/${id}`]);
+            return false;
+          }
+        });
+      });
+    }
   
 }
