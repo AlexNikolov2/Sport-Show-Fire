e3e5765c7d9f481dc58468f32afc95e0dc14be97
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-15 14:59:11 +0300

    build

--- sport-show-forum/src/app/shared/shared.module.ts
+++ sport-show-forum/src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShortenPipe } from './pipes/shorten.pipe';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
     ShortenPipe,
-    NotFoundComponent,
   ],
   imports: [
     CommonModule
