e199e021b6c222983ed16e910e9800024e138844
Aleks Nikolov <tcr.official.world@gmail.com>
2022-04-16 09:49:09 +0300

    code refactoring

--- sport-show-forum/src/app/items/all-posts/all-posts.component.ts
+++ sport-show-forum/src/app/items/all-posts/all-posts.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { PostService } from 'src/app/core/services/post.service';
-import { Post } from 'src/app/shared/interface/post';
 @Component({
   selector: 'app-all-posts',
   templateUrl: './all-posts.component.html',
